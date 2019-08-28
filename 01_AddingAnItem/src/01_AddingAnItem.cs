ce9a60ef10a5f96ac11663417ab697831903af4c
Zatherz <zatherz@linux.pl>
2019-08-28 12:36:50 +0200

    Initial commit

---
+++ 01_AddingAnItem/src/01_AddingAnItem.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Semi;
+using Logger = ModTheGungeon.Logger;
+
+namespace AddingAnItem {
+	public class AddingAnItem : Mod {
+		public Logger Logger = new Logger("01_AddingAnItem");
+
+		public override void Loaded() {
+			Logger.Info($"Hello world!");
+		}
+
+		public override void RegisterContent() {
+			Logger.Debug($"Registering content.");
+
+			var def = RegisterEncounterIcon("@:my_item_icon", "my_item.png");
+			RegisterSpriteCollection(
+				"@:my_item_coll",
+				def
+			);
+			RegisterSpriteTemplate(
+				"@:my_item_sprite",
+				"@:my_item_coll",
+				"@:my_item_icon"
+			);
+			RegisterLocalization(
+				"@:english_items",
+				"english_items.txt",
+				"gungeon:english",
+				I18N.StringTable.Items
+			);
+
+			RegisterItem<MyItem>(
+				"@:my_item",
+				"@:my_item_icon",
+				"@:my_item_sprite",
+				"#@:MY_ITEM_NAME",
+				"#@:MY_ITEM_SHORT_DESC",
+				"#@:MY_ITEM_LONG_DESC"
+			);
+		}
+
+		public override void InitializeContent() {
+			Logger.Debug($"Initializing content.");
+		}
+	}
+}
---
+++ 01_AddingAnItem/src/MyItem.cs
@@ -0,0 +1,24 @@
+﻿using System;
+namespace AddingAnItem {
+	public class MyItem : PassiveItem {
+		public StatModifier Modifier = new StatModifier {
+			statToBoost = PlayerStats.StatType.Health,
+			amount = 2,
+			modifyType = StatModifier.ModifyMethod.MULTIPLICATIVE
+		};
+
+		public override void Pickup(PlayerController player) {
+			base.Pickup(player);
+
+			player.ownerlessStatModifiers.Add(Modifier);
+			player.stats.RecalculateStats(player);
+		}
+
+		public override DebrisObject Drop(PlayerController player) {
+			player.ownerlessStatModifiers.Remove(Modifier);
+			player.stats.RecalculateStats(player);
+
+			return base.Drop(player);
+		}
+	}
+}
